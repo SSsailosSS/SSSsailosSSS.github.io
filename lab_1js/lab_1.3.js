426b83259e256675eb03cacc11676f059d132073
SSsailosSS <106996847+SSsailosSS@users.noreply.github.com>
2023-06-09 13:17:20 +0300

    Update lab_1.3.js

--- lab_1js/lab_1.3.js
+++ lab_1js/lab_1.3.js
@@ -1,31 +1,6 @@
 'use strict';
-function number() {
-  let number;
+let num;
 
-  let num;
-  do {
-      num = prompt('Введите число большее 100');
-      if (num === null) return; // выход из функции, если нажали "Отмена"
-      if (num === '') continue; // продолжаем цикл, если введена пустая строка
-  } while (num <= 100 ||  isNaN(num));
-  return ('Вы ввели ', num, '> 100');
-}
-
-function simple() {
-  let n = prompt("Введите число:");
-
-  for (let i = 2; i <= n; i++) {
-    let isPrime = true;
-
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-
-    if (isPrime) {
-      console.log(i);
-    }
-  }
-}
+do {
+  num = prompt("Введите число больше 100");
+} while (num !== null && (isNaN(num) || +num <= 100));
